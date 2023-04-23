8d8ff0a9d82a8ea19dd52a845652936f44e9a904
Melanie S <melanie.sapiandante@gmail.com>
2023-04-22 20:32:51 -0400

    worked on schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+-- Creates the employee database --
+DROP DATABASE IF EXISTS littlespoon_db;
+CREATE DATABASE littlespoon_db;
+-- Uses the littlespoon_db database --
+USE littlespoon_db;
+
+-- Selects littlespoon_db -- 
+SELECT DATABASE();
