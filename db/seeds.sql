7e756fcb8f517073582d9e2683ff3d1c769d06ce
Melanie S <melanie.sapiandante@gmail.com>
2023-04-24 20:32:09 -0400

    added to seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,15 +1,19 @@
 INSERT INTO Department (id, name)
-VALUES (Front of house),
-(Back of house)
+VALUES (FOH),
+(BOH)
 (Management);
 
 INSERT INTO Role (id, title, salary, department_id)
-VALUES (),
-(),
-(),
-(),
-(),
-();
+VALUES (Co-Owner, 50,000.00, management),
+(Co-owner, 50,000.00, management),
+(server, 24,000.00, FOH),
+(barista, 19,000.00, FOH),
+(host, 17,000.00, FOH),
+(Chef, 31,000.00, BOH),
+(Sous-Chef, 25,000.00, BOH),
+(Line-cook, 22,000.00, BOH),
+(Prep-cook, 20,000.00, BOH),
+(Dishwasher, 18,000.00, BOH);
 
 INSERT INTO Employee (id, first_name, last_name, role_id, manager_id)
 VALUES (),
