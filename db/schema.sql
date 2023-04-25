a1e735a43dc5a8ca9d2a8016fa84ac1c817b0574
Melanie S <melanie.sapiandante@gmail.com>
2023-04-24 21:30:12 -0400

    debugged seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -8,32 +8,27 @@ USE littlespoon_db;
 SELECT DATABASE();
 
 CREATE TABLE Department (
-
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     Name  VARCHAR(30)
 );
 
 CREATE TABLE Role (
-  
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title   VARCHAR(30),
-    salary  DECIMAL,
-    department_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT NOT NULL,
     FOREIGN KEY (department_id)
-    REFERENCES department(id)
-    ON DELETE SET NULL --follow up and check if this is needed-- 
+    REFERENCES Department(id)
 );
 
-CREATE TABLE Employeee (
-    id  NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name      VARCHAR(30),
-    last_name       VARCHAR(30),
-    role_id         INT,
-    manager_id      INT,
+CREATE TABLE Employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
     FOREIGN KEY (role_id)
-    REFERENCES role(id),
+    REFERENCES Role(id),
     FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
-
-    ON DELETE SET NULL-- not sure if this is needed, follow up--
+    REFERENCES Employee(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,28 +1,27 @@
-INSERT INTO Department (id, name)
-VALUES (FOH),
-(BOH)
-(Management);
+INSERT INTO Department (name)
+VALUES ("FOH"),
+       ("BOH"),
+       ("Management");
 
-INSERT INTO Role (id, title, salary, department_id)
-VALUES (Co-Owner, 50,000.00, management),
-(Co-owner, 50,000.00, management),
-(server, 24,000.00, FOH),
-(barista, 19,000.00, FOH),
-(host, 19,000.00, FOH),
-(Chef, 31,000.00, BOH),
-(Sous-Chef, 25,000.00, BOH),
-(Line-cook, 22,000.00, BOH),
-(Prep-cook, 20,000.00, BOH),
-(Dishwasher, 18,000.00, BOH);
+INSERT INTO Role (title, salary, department_id)
+VALUES ("Co-Owner", 50000.00, 3),
+       ("Server", 24000.00, 1),
+       ("Barista", 19000.00, 1),
+       ("Host", 19000.00, 1),
+       ("Chef", 31000.00, 2),
+       ("Sous-Chef", 25000.00, 2),
+       ("Line-cook", 22000.00, 2),
+       ("Prep-cook", 20000.00, 2),
+       ("Dishwasher", 18000.00, 2);
 
-INSERT INTO Employee (id, first_name, last_name, role_id, manager_id)
-VALUES ( Jeanne, French, 1, null),
-(Erin, DelBaggio, 2, null),
-(Al, Libertine, 3, 1),
-(Em, Crews, 4, 1),
-(Tatum, Dilks, 5, 1),
-(Yara, Saad, 6, 2 );
-(Miki, Sapiandante, 7, 2), 
-(Cody, Brown, 8, 2),
-(Sara, Lopez, 9, 2),
-(Kyle, Daniels, 10, 2); 
+INSERT INTO Employee (first_name, last_name, role_id, manager_id)
+VALUES ("Jeanne", "French", 1, null),
+       ("Erin", "DelBaggio", 2, null),
+       ("Al", "Libertine", 3, 1),
+       ("Em", "Crews", 4, 1),
+       ("Tatum", "Dilks", 5, 1),
+       ("Yara", "Saad", 6, 2),
+       ("Miki", "Sapiandante", 7, 2), 
+       ("Cody", "Brown", 8, 2),
+       ("Sara", "Lopez", 9, 2),
+       ("Kyle", "Daniels", 10, 2);
